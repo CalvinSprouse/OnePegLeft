b83aca4ba84d41af2405f3553ddd31026b05a059
CalvinSprouse <sprousecal@gmail.com>
2021-07-18 10:24:05 -0500

    init

---
+++ one_peg.py
@@ -0,0 +1,5 @@
+"""
+A triangle board can instead be a hexagonal board
+Ajdacent hexagons follow hexagonal math
+https://stackoverflow.com/questions/11373122/best-way-to-store-a-triangular-hexagonal-grid-in-python
+"""
